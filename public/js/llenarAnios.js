98bed549660eafe980a7fc17db51637a8400d295
Zhertal <tumburulumbu@gmail.com>
2018-06-06 12:34:03 -0500

    Eliminados archivos inservibles, corregido registro mal redireccionado

--- public/js/llenarAnios.js
+++
@@ -1,6 +0,0 @@
-<?php 
-echo "<option selected value='0'>Año</option>";
-for ($i=1950; $i < 2019 ; $i++) { 
-	echo "<option value='".$i."'>".$i."</option>";
-}
- ?>
--- public/js/llenarMeses.js
+++
@@ -1,7 +0,0 @@
-<?php
-$meses = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
-echo "<option selected value='0'>Mes</option>";
-for ($i=1; $i <13 ; $i++) { 
-	echo "<option value='".$i."'>".$meses[$i-1]."</option>";
-}
- ?>
