63060f45c88260aa685166e387c001f5627278ac
Garo <ebalderas03@alumnos.uaq.mx>
2018-06-06 18:33:01 -0500

    Base de datos actualizada

--- files/standort.sql
+++ files/standort.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.4.12
+-- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-06-2018 a las 04:25:26
--- Versión del servidor: 10.1.28-MariaDB
--- Versión de PHP: 7.1.11
+-- Tiempo de generación: 07-06-2018 a las 01:32:18
+-- Versión del servidor: 5.6.25
+-- Versión de PHP: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `standort`
 --
-CREATE DATABASE IF NOT EXISTS `standort` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `standort`;
 
 -- --------------------------------------------------------
 
@@ -30,7 +26,7 @@ USE `standort`;
 -- Estructura de tabla para la tabla `categoria`
 --
 
-CREATE TABLE `categoria` (
+CREATE TABLE IF NOT EXISTS `categoria` (
   `idCategoria` int(6) NOT NULL,
   `categoria` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -41,13 +37,26 @@ CREATE TABLE `categoria` (
 -- Estructura de tabla para la tabla `comentarios`
 --
 
-CREATE TABLE `comentarios` (
+CREATE TABLE IF NOT EXISTS `comentarios` (
   `idComentario` int(8) NOT NULL,
   `idUsuario` int(6) NOT NULL,
   `idSitio` int(6) NOT NULL,
   `comentario` varchar(600) NOT NULL,
   `fecha` date NOT NULL,
-  `kudos` int(3) NOT NULL
+  `kudos` int(3) NOT NULL,
+  `calificacion` decimal(2,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE IF NOT EXISTS `favoritos` (
+  `idFavorito` int(11) NOT NULL,
+  `idSitio` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -56,7 +65,7 @@ CREATE TABLE `comentarios` (
 -- Estructura de tabla para la tabla `sitio`
 --
 
-CREATE TABLE `sitio` (
+CREATE TABLE IF NOT EXISTS `sitio` (
   `idSitio` int(6) NOT NULL,
   `idUsuario` int(6) NOT NULL,
   `nombreSitio` varchar(200) NOT NULL,
@@ -72,7 +81,7 @@ CREATE TABLE `sitio` (
 -- Estructura de tabla para la tabla `sitiocategoria`
 --
 
-CREATE TABLE `sitiocategoria` (
+CREATE TABLE IF NOT EXISTS `sitiocategoria` (
   `idSitioCategoria` int(6) NOT NULL,
   `idSitio` int(6) NOT NULL,
   `idCategoria` int(6) NOT NULL
@@ -84,7 +93,7 @@ CREATE TABLE `sitiocategoria` (
 -- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `usuario` (
+CREATE TABLE IF NOT EXISTS `usuario` (
   `idUsuario` int(6) NOT NULL,
   `correo` varchar(120) NOT NULL,
   `password` varchar(300) NOT NULL,
@@ -110,6 +119,12 @@ ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`idComentario`),
   ADD UNIQUE KEY `idUsuario` (`idUsuario`),
   ADD UNIQUE KEY `idSitio` (`idSitio`);
+
+--
+-- Indices de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`idFavorito`);
 
 --
 -- Indices de la tabla `sitio`
@@ -141,31 +156,31 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `categoria`
   MODIFY `idCategoria` int(6) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
   MODIFY `idComentario` int(8) NOT NULL AUTO_INCREMENT;
-
+--
+-- AUTO_INCREMENT de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `idFavorito` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `sitio`
 --
 ALTER TABLE `sitio`
   MODIFY `idSitio` int(6) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT de la tabla `sitiocategoria`
 --
 ALTER TABLE `sitiocategoria`
   MODIFY `idSitioCategoria` int(6) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `idUsuario` int(6) NOT NULL AUTO_INCREMENT;
-
 --
 -- Restricciones para tablas volcadas
 --
@@ -189,7 +204,6 @@ ALTER TABLE `sitio`
 ALTER TABLE `sitiocategoria`
   ADD CONSTRAINT `sitiocategoria_ibfk_1` FOREIGN KEY (`idSitio`) REFERENCES `sitio` (`idSitio`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `sitiocategoria_ibfk_2` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
