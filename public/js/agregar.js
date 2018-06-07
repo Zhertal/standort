68eb2fd94317f567f0af9428a763e30ed9259079
Zhertal <tumburulumbu@gmail.com>
2018-06-07 04:20:21 -0500

    Mapa funcional

--- public/js/agregar.js
+++ public/js/agregar.js
@@ -1,24 +1,19 @@
-window.addEventListener('load',inicializar, true);
+/*Window.onload = inicializar;
 
 function inicializar(){
 	verMapa();
 }
 
 function verMapa(){
-	navigator.geolocation.getCurrentPosition(lugar)
+	navigator.geolocation.getCurrentPosition(lugar);
 }
 
 function lugar(coordenadas){
-	longitud = coordenadas.coords.longitude;
-	latitud = coordenadas.coords.latitude;
+	var longitud = coordenadas.coords.longitude;
+	var latitud = coordenadas.coords.latitude;
 	console.log(longitud, latitud);
 	initMap();
-}
-
-
-
-
-
+}*/
 
 function cambio(page){
 	location.href = page;
